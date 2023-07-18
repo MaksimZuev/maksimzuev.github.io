d63667d76287f6c8e078342e613e882dc9bf106a
Maksim Zuev <zuev.maksim8@wb.ru>
2023-07-19 01:41:55 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/maksimzuev/'
+  publicPath: '/maksimzuev.github.io/'
 })
